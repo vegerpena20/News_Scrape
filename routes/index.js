94578be5d95b720e32a053cf26661f0afb78119d
Veger Pena <vegerpena@ymail.com>
2020-01-10 04:01:56 -0500

    routes code

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+//Not complete/not working
+var router = require("express").Router();
+var apiRoutes = require("./api");
+var viewRoutes = require("./view");
+
+router.use("/api", apiRoutes);
+router.use("/", viewRoutes);
+
+module.exports = router;
